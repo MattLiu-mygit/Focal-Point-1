1780da74793eb8e958e253db84430ac4fb90b80b
James S. Wang <JJamesWWang@gmail.com>
2020-05-19 16:53:44 -0400

    Make ringgun rotate in a way that's more helpful to the player

--- weapons/guns/ring/RingGun.gd
+++ weapons/guns/ring/RingGun.gd
@@ -9,21 +9,33 @@ const RingBullet := preload("res://weapons/bullets/ring/RingBullet.tscn")
 func set_gun_rotation(_mouse_angle: float) -> void:
 	var mouse_position = get_parent().get_local_mouse_position()
 	if mouse_position.y <= 0:
-		rotation = deg2rad(-90)
+		rotation = deg2rad(-90 + gun_rotation * 90)
+		if mouse_position.x >= 0:
+			rotation += deg2rad(15)
+		else:
+			rotation += deg2rad(-15)
 	else:
-		rotation = deg2rad(90)
+		rotation = deg2rad(90 + gun_rotation * 90)
+		if mouse_position.x >= 0:
+			rotation += deg2rad(-15)
+		else:
+			rotation += deg2rad(15)
+	
 
 
 func instance_bullet(Bullet_: PackedScene) -> Bullet:
 	var bullet := .instance_bullet(Bullet_)
 
-	bullet.center = Vector2(get_global_mouse_position().x, global_position.y)
+	# We only use the global x position for the radius, y is the y-coord of the gun
+	bullet.center = (global_position + 
+			(Vector2(get_global_mouse_position().x, global_position.y) - 
+			global_position).rotated(deg2rad(gun_rotation * 90)))
 	var mouse_position = get_parent().get_local_mouse_position()
 	if mouse_position.y <= 0:
 		# 1.4 is a magic number that makes this work
-		bullet.motion = Vector2(0, -abs(mouse_position.y) * 1.4)
+		bullet.motion = Vector2(0, -abs(mouse_position.y) * 1.4).rotated(deg2rad(gun_rotation * 90))
 	else:
-		bullet.motion = Vector2(0, abs(mouse_position.y) * 1.4)		
+		bullet.motion = Vector2(0, abs(mouse_position.y) * 1.4).rotated(deg2rad(gun_rotation * 90))
 	return bullet
 
 
