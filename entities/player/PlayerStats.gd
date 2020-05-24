dcb0372426d288dab948eaf924f45d172db1229a
James S. Wang <JJamesWWang@gmail.com>
2020-05-24 13:08:55 -0400

    Add some basic starter code for swapping weapons

--- entities/player/PlayerStats.gd
+++ entities/player/PlayerStats.gd
@@ -1,10 +1,13 @@
 extends Resource
 class_name PlayerStats
-
+# This class contains all of the Player's relevant game data.
 
 export (int) var max_health := 3
 var health := max_health setget set_health
-
+var ring_gun_unlocked := false
+var reflect_gun_unlocked := false
+var rotate_gun_unlocked := false
+var laser_sights_unlocked := false
 
 func set_health(value: int):
 	# warning-ignore:narrowing_conversion
