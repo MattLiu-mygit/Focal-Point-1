6bdf10719cdeae71b5bbdd22a940c70969b37774
MattLiu-mygit <65636118+MattLiu-mygit@users.noreply.github.com>
2020-05-29 02:17:11 -0400

    Updated world

--- world/World.gd
+++ world/World.gd
@@ -1,7 +1,6 @@
 extends Node2D
 # The World is the level loader and the scene where everything takes place.
 
-onready var camera : Camera2D = $Camera
 onready var player : Player = $Player
 
 var mouse_cursor = load("res://MouseCursor.png")
@@ -9,8 +8,3 @@ var mouse_cursor = load("res://MouseCursor.png")
 func _ready() -> void:
 	VisualServer.set_default_clear_color(Color.black)
 	Input.set_custom_mouse_cursor(mouse_cursor)
-
-
-func _process(_delta: float) -> void:
-	# TODO: Cases where player moves out of camera's y-frame
-	camera.global_position.x = player.global_position.x
