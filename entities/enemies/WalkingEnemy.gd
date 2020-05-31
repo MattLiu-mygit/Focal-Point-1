1af913ac0830903a3f16f566395d9b80d927b6bd
JJamesWWang <JJamesWWang@gmail.com>
2020-05-31 19:43:35 -0400

    Make ShieldEnemy flip sprite based on mouse, revise patrolling

--- entities/enemies/WalkingEnemy.gd
+++ entities/enemies/WalkingEnemy.gd
@@ -5,7 +5,7 @@ enum DIRECTION {LEFT = -1, RIGHT = 1}
 export(DIRECTION) var WALKING_DIRECTION = DIRECTION.LEFT
 export(int) var LEFT_BOUND
 export(int) var RIGHT_BOUND
-export(int) var pause_time = 3
+export(float) var PAUSE_TIME = 1.0
 
 var start_x
 var motion_x
@@ -13,8 +13,9 @@ var motion_x
 onready var sprite: Sprite = $Sprite
 onready var floor_left: RayCast2D = $FloorLeft
 onready var floor_right: RayCast2D = $FloorRight
-onready var wall: RayCast2D = $Wall
-onready var timer: Timer = $PatrolTimer
+onready var wall_left: RayCast2D = $WallLeft
+onready var wall_right: RayCast2D = $WallRight
+onready var patrol_timer: Timer = $PatrolTimer
 onready var hurtbox: Area2D = $Hurtbox
 onready var collider: Area2D = $Collider
 
@@ -22,22 +23,23 @@ onready var collider: Area2D = $Collider
 func _ready():
 	start_x = position.x
 	motion.x = SPEED * WALKING_DIRECTION
+	patrol_timer.wait_time = PAUSE_TIME
 
 
-# Basically rotates the beam so that it's pointed at the mouse.
 func _process(_delta: float) -> void:
-	sprite.rotation = get_local_mouse_position().angle() + deg2rad(90)
-	hurtbox.rotation = get_local_mouse_position().angle() + deg2rad(90)
-	collider.rotation = get_local_mouse_position().angle() + deg2rad(90)
+	var facing_direction := sign(get_local_mouse_position().x)
+	if facing_direction != 0:
+		sprite.scale.x = facing_direction
 
 
 func _physics_process(_delta: float) -> void:
-	if not floor_right.is_colliding() or not floor_left.is_colliding() or wall.is_colliding() or not in_patrol_area():
-		WALKING_DIRECTION *= -1
-		motion.x = 0
-		scale.x *= -1
-		timer.start()
-	motion = move_and_slide_with_snap(motion, Vector2.DOWN * 8, Vector2.UP, true, 4, deg2rad(46))
+	motion = move_and_slide(motion, Vector2.UP)
+	if (not floor_right.is_colliding() or not floor_left.is_colliding() or 
+			wall_left.is_colliding() or wall_right.is_colliding()) or not in_patrol_area():
+		if motion.x != 0:
+			WALKING_DIRECTION *= -1
+			motion.x = 0
+			patrol_timer.start()
 
 
 func in_patrol_area() -> bool:
