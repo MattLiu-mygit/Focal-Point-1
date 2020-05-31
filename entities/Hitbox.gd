bd55d5115b44bf659b7b2b9e8c8a489e95169921
James S. Wang <jjameswwang@gmail.com>
2020-05-30 21:53:00 -0400

    Update Hitbox.gd

--- entities/Hitbox.gd
+++ entities/Hitbox.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 export (int) var DAMAGE
 
+
 # Hitboxes should only enter Hurtboxes, so we assume the parameter is a hurtbox.
 func _on_area_entered(hurtbox: Area2D) -> void:
 	hurtbox.emit_signal("hit", DAMAGE)
