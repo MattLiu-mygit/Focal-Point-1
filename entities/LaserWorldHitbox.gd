16362796d0ddebbe3bbd8477c5c88b884c81e25d
MatthewL <mliu78.jh.edu>
2020-05-31 02:49:11 -0400

    fixed bugs with laser enemy, made it so that patrolling enemy stops after going his round.

--- entities/LaserWorldHitbox.gd
+++ entities/LaserWorldHitbox.gd
@@ -6,15 +6,15 @@ var colliding = false
 onready var collider : CollisionShape2D = $Collider
 # Gets the scale of the laser sprite.
 onready var laser_scale := get_parent().get_parent().get_parent()
-onready var original_size : int = laser_scale.scale.y
+onready var original_size : float = laser_scale.scale.y
 
 
 # When colliding, laser scale sheaths. When not colliding, laser increases length.
 func _process(_delta) -> void:
-	if not colliding and laser_scale.scale.y < original_size*0.8:
+	if not colliding and laser_scale.scale.y < original_size:
 		laser_scale.scale.y += 0.4
 		
-	if not colliding and laser_scale.scale.y > 10:
+	if colliding and laser_scale.scale.y > 10:
 		laser_scale.scale.y -= 1
 
 
--- entities/enemies/shield/ShieldEnemy.gd
+++ entities/enemies/shield/ShieldEnemy.gd
@@ -1,31 +1,39 @@
 extends Enemy
 
-enum DIRECTION {LEFT = -1, RIGHT = 1}
+enum DIRECTION {LEFT = 1, RIGHT = -1}
 
 export(DIRECTION) var WALKING_DIRECTION = DIRECTION.LEFT
 export(int) var LEFT_BOUND
 export(int) var RIGHT_BOUND
-export(int) var start_x = position.x
+export(int) var pause_time = 3
 
-var state
+var start_x
+var motion_x
 
 onready var sprite : Sprite = $Sprite
 onready var floor_left: RayCast2D = $FloorLeft
 onready var floor_right: RayCast2D = $FloorRight
 onready var wall : RayCast2D = $Wall
+onready var timer : Timer = $PatrolTimer
 
 
 func _ready():
 	start_x = position.x
-	state = WALKING_DIRECTION
 	motion.x = SPEED * WALKING_DIRECTION
+	scale.x *= -1
 
 
 func _physics_process(_delta) -> void:
+	var turned = false
 	if not floor_right.is_colliding() or not floor_left.is_colliding() or wall.is_colliding() or not in_patrol_area():
-		state *= -1
-		motion.x *= -1
 		scale.x *= -1
+		WALKING_DIRECTION *= -1
+		turned = true
+		
+	if turned:
+		motion.x = 0
+		timer.start()
+	
 	motion = move_and_slide_with_snap(motion, Vector2.DOWN * 8, Vector2.UP, true, 4, deg2rad(46))
 
 
@@ -37,3 +45,7 @@ func in_patrol_area() -> bool:
 		return true
 	else:
 		return false
+
+
+func _on_PatrolTimer_timeout() -> void:
+	motion.x = SPEED * WALKING_DIRECTION
