0d6d27866aaece5f2b27f30a8f32180efe3969b5
James S. Wang <jjameswwang@gmail.com>
2020-05-30 21:50:48 -0400

    Test editing through pull request

--- entities/Hitbox.gd
+++ entities/Hitbox.gd
@@ -3,5 +3,5 @@ extends Area2D
 export (int) var DAMAGE
 
 # Hitboxes should only enter Hurtboxes, so we assume the parameter is a hurtbox.
-func _on_area_entered(hurtbox: Area2D):
+func _on_area_entered(hurtbox: Area2D) -> void:
 	hurtbox.emit_signal("hit", DAMAGE)
