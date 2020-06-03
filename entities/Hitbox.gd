2229dcc40eca44ae38659312e31d4c4775536834
JJamesWWang <JJamesWWang@gmail.com>
2020-06-02 20:57:54 -0400

    Add position of hitbox to hit signal

--- entities/Hitbox.gd
+++ entities/Hitbox.gd
@@ -4,5 +4,6 @@ export (int) var DAMAGE
 
 
 # Hitboxes should only enter Hurtboxes, so we assume the parameter is a hurtbox.
+# We use global_position because local position doesn't tell us anything.
 func _on_area_entered(hurtbox: Area2D) -> void:
-	hurtbox.emit_signal("hit", DAMAGE)
+	hurtbox.emit_signal("hit", DAMAGE, global_position)
