5ebe2941682bf32886265cec7b04cfe78b92b3bc
MatthewL <mliu78.jh.edu>
2020-06-03 23:24:05 -0400

    Ghost now fades and reappears. When it's transparent or disappearing, players can't hurt it. When it's solid, players can hit it.

--- entities/enemies/ghost/GhostEnemy.gd
+++ entities/enemies/ghost/GhostEnemy.gd
@@ -1,8 +1,12 @@
 extends Enemy
 
-enum DIRECTION {LEFT = -1, RIGHT = 1}
+# Maybe make the enemy keyframe be just the eyes when in walls
+# fade in and out. When faded, hurtbox 0
 
-export(DIRECTION) var WALKING_DIRECTION = DIRECTION.LEFT
+enum DIRECTION {LEFT = -1, RIGHT = 1}
+enum STATE {SEMISOLID = 1, SOLID = 0, TRANSPARENT = -1}
+
+export(DIRECTION) var WALKING_DIRECTION = DIRECTION.RIGHT
 export(int) var LEFT_BOUND
 export(int) var RIGHT_BOUND
 export(int) var pause_time = 3
@@ -10,16 +14,22 @@ export(int) var SPOOPYNESS = 1
 
 var start_x
 var motion_x
-var motion_up = false
+var motion_up
+var state
 
 onready var sprite: Sprite = $Sprite
 onready var hurtbox: Area2D = $Hurtbox
 onready var collider: Area2D = $Collider
 onready var patrol_timer: Timer = $PatrolTimer
 onready var spoopy_timer: Timer = $SpoopyTimer
+onready var fade_timer: Timer = $FadeTimer
+onready var wall: RayCast2D = $Wall
+onready var fade_animator: AnimationPlayer = $FadeAnimator
 
 
 func _ready() -> void:
+	motion_up = false
+	state = STATE.TRANSPARENT
 	start_x = position.x
 	motion.x = SPEED * WALKING_DIRECTION
 	patrol_timer.wait_time = pause_time
@@ -27,7 +37,6 @@ func _ready() -> void:
 
 
 # Checks for two cases in which special action needs to occur: 
-# NTS: USE DELTA
 func _physics_process(_delta: float) -> void:
 	if not in_patrol_area():
 		return_to_patrol_area()
@@ -49,7 +58,7 @@ func patrol_flip() -> void:
 	WALKING_DIRECTION *= -1
 	motion.x = 0
 	patrol_timer.start()
-	
+	scale.x *= -1
 
 # Checks if taking one step would take the enemy away or towards the patrol area.
 func check_direction() -> bool:
@@ -72,6 +81,17 @@ func in_patrol_area() -> bool:
 		return false
 
 
+func fade() -> void:
+	fade_animator.play("fade")
+	hurtbox.set_collision_layer_bit(4, false)
+	state = STATE.TRANSPARENT
+
+func materialize() -> void:
+	fade_animator.play_backwards("fade")
+	hurtbox.set_collision_layer_bit(4, true)
+	state = STATE.SOLID
+
+
 func _on_PatrolTimer_timeout() -> void:
 	motion.x = SPEED * WALKING_DIRECTION
 
@@ -84,3 +104,15 @@ func _on_SpoopyTimer_timeout() -> void:
 	else:
 		motion.y *= -1
 		motion_up = true
+
+
+func _on_FadeTimer_timeout() -> void:
+	if state == STATE.TRANSPARENT:
+		state = STATE.SEMISOLID
+	elif state == STATE.SOLID:
+		state = STATE.SEMISOLID
+	else:
+		if hurtbox.get_collision_layer_bit(4) == false:		
+			materialize()
+		else: 
+			fade()
