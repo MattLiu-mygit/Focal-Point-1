6e943e1c4b738b4caa77b69a671d1405bd945387
MatthewL <mliu78.jh.edu>
2020-06-06 21:15:33 -0400

    Helicopter given two raycasts to display patrol range. There seems to be a bug where bullets don't queuefree when hitting shield and an error is given for gun where set_wait_time gives an error

--- entities/enemies/homing/HomingEnemy.gd
+++ entities/enemies/homing/HomingEnemy.gd
@@ -16,7 +16,7 @@ func _ready() -> void:
 	motion.x = SPEED * WALKING_DIRECTION
 
 
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	var player = main_instances.player
 	
 	if player != null:
