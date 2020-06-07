55ee9061281f12810f2e205e2cc7085b432c991d
JJamesWWang <JJamesWWang@gmail.com>
2020-06-07 00:50:50 -0400

    Implement Entity base functions

--- entities/enemies/Enemy.gd
+++ entities/enemies/Enemy.gd
@@ -1,4 +1,4 @@
-extends KinematicBody2D
+extends "res://entities/Entity.gd"
 class_name Enemy
 
 signal enemy_died
@@ -11,14 +11,18 @@ var motion = Vector2.ZERO
 onready var stats = $EnemyStats
 
 
+func hit(damage: int, _spot: Vector2) -> void:
+	stats.health -= damage
+
+
 func die() -> void:
 	queue_free()
+	emit_signal("enemy_died")
 
 
-func _on_Hurtbox_hit(damage: int, _spot: Vector2) -> void:
-	stats.health -= damage
+func _on_Hurtbox_hit(damage: int, spot: Vector2) -> void:
+	hit(damage, spot)
 
 
 func _on_EnemyStats_enemy_died() -> void:
 	die()
-	emit_signal("enemy_died")
