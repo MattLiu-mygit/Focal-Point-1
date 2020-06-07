71549c84492478f79594b5ecc97f3882cdaa05f8
JJamesWWang <JJamesWWang@gmail.com>
2020-06-07 15:20:32 -0400

    Add key icon and signal for has_key changing

--- entities/player/PlayerStats.gd
+++ entities/player/PlayerStats.gd
@@ -4,6 +4,7 @@ class_name PlayerStats
 
 signal player_health_changed(health)
 signal player_total_health_changed(total_health)
+signal player_has_key_changed(has_key)
 # warning-ignore:unused_signal
 signal player_fell    # emitted by KillZone
 signal player_died(total_health, max_health)
@@ -13,7 +14,7 @@ var total_health := 99 setget set_total_health
 var max_health := 3
 var health := max_health setget set_health
 var selected_level: PackedScene = null
-var has_key := false
+var has_key := false setget set_has_key
 # Various unlockable perks
 var basic_gun_unlocked := true
 var ring_gun_unlocked := false
@@ -36,3 +37,8 @@ func set_total_health(value: int):
 	# warning-ignore:narrowing_conversion
 	total_health = clamp(value, 0, 99)
 	emit_signal("player_total_health_changed", total_health)
+
+
+func set_has_key(value: bool):
+	has_key = value
+	emit_signal("player_has_key_changed", value)
