fa64917f3665638b3b6d8042d144f11a6867e8aa
Matthew Liu <mliu78@jh.edu>
2020-06-08 12:19:15 -0400

    Updated world. Changed hole enemy so that the killzone kills it vs. the fall kill timer when falling. Added visible and changing patrol paths for flying enemies that can be viewed when played in the testing room with 'Visible collision shapes' turned on in Debug settings

--- entities/enemies/floor/hole/HoleEnemy.gd
+++ entities/enemies/floor/hole/HoleEnemy.gd
@@ -7,8 +7,6 @@ export (int) var DEPTH_BY_TILE = 1
 var main_instances = ResourceLoader.main_instances
 var on_player = false
 
-onready var fall_kill_timer = $FallKillTimer
-
 
 func follow_mouse() -> void:
 	var player = main_instances.player
@@ -26,10 +24,9 @@ func follow_mouse() -> void:
 		
 		# Once the player clears one layer of tileset, reinstate player world
 		# collsion.
-		if player.position.y > position.y + 25 + (DEPTH_BY_TILE * 16):
+		if player.position.y > position.y + 25 + (DEPTH_BY_TILE * 16) and on_player:
 			on_player = false
 			player.set_collision_mask_bit(0, true)
-			fall_kill_timer.start()
 
 
 func _on_Hitbox_area_entered(_area: Area2D) -> void:
@@ -38,12 +35,3 @@ func _on_Hitbox_area_entered(_area: Area2D) -> void:
 
 func _on_Hitbox_body_entered(_body: Node) -> void:
 	on_player = true
-
-
-# If the player falls for more than 3 seconds and has hit terminal speed, it's
-# safe to assume it died or the fall would kill him. Also there shouldn't be
-# any 3 second falls
-func _on_FallKillTimer_timeout() -> void:
-	var player = main_instances.player
-	if player.motion.y == player.TERMINAL_SPEED:
-		player.stats.health -= player.stats.health
