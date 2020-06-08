e4161c4faf7a735a51ec4651e8e054c1f5b4566a
JJamesWWang <JJamesWWang@gmail.com>
2020-06-08 17:43:46 -0400

    Emit signal player_cleared_room when entering door

--- world/environment/Door.gd
+++ world/environment/Door.gd
@@ -1,8 +1,8 @@
 extends Area2D
 
-var world: Node = ResourceLoader.main_instances.world
+var player_stats = ResourceLoader.player_stats
 
 
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
-		world.set_room(world.room.next_room)
+		player_stats.emit_signal("player_cleared_room")
